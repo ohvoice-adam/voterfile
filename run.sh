fb617bf3c0e2e22f7a22fbba60a8b9dfdc47a0e8
Adam Parsons <adam@rosa.lenin.party>
2018-07-23 23:09:35 -0400

    Fix db loading

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TODAY=$(date +%Y%m%d)
-cd /home/$USER/voters
+cd /home/$USER/voterfile
 ./download.sh >> logs/log_$TODAY.txt 2>&1
 ./check.sh >> logs/log_$TODAY.txt 2>&1
+./process.sh >> logs/log_$TODAY.txt 2>&1
+./ec2_process.sh >> logs/log_$TODAY.txt 2>&1
